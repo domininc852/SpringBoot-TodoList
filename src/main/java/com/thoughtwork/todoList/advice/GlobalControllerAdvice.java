6cf5236ec2cdf801e06a4361cb83aebeb8c969ef
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-12-10 18:28:12 +0800

    [Spring-TodoList][Dominic Leung] feat: add two handler in global controller device to handle label not found exception and todo item not found exception

--- src/main/java/com/thoughtwork/todoList/advice/GlobalControllerAdvice.java
+++ src/main/java/com/thoughtwork/todoList/advice/GlobalControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.thoughtwork.todoList.advice;
 
+import com.thoughtwork.todoList.exceptions.LabelNotFoundException;
+import com.thoughtwork.todoList.exceptions.TodoItemNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,5 +13,16 @@ public class GlobalControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadRequest(IllegalArgumentException exception){
         return new ErrorResponse(exception.getMessage(),HttpStatus.BAD_REQUEST.name());
+    }
+
+    @ExceptionHandler(LabelNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleLabelNotFound(LabelNotFoundException exception){
+        return new ErrorResponse(exception.getMessage(),HttpStatus.NOT_FOUND.name());
+    }
+    @ExceptionHandler(TodoItemNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleTodoItemNotFound(TodoItemNotFoundException exception){
+        return new ErrorResponse(exception.getMessage(),HttpStatus.NOT_FOUND.name());
     }
 }
