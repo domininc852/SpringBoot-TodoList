14f4f59b96679978dad9a35a7567957b0b64ca8f
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-12-10 16:30:53 +0800

    [Spring-TodoList][Dominic Leung] test: add test for add one todo item

--- src/test/java/com/thoughtwork/todoList/ServiceTest/TodoListServiceTest.java
+++ src/test/java/com/thoughtwork/todoList/ServiceTest/TodoListServiceTest.java
@@ -7,6 +7,7 @@ import com.thoughtwork.todoList.services.TodoListService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,12 +19,25 @@ public class TodoListServiceTest {
         //given
         TodoListRepository todoListRepository = Mockito.mock(TodoListRepository.class);
         TodoListService todoListService = new TodoListService(todoListRepository);
-        List<Label> labels = Arrays.asList(new Label("1", "shopping", "white"));
-        List<TodoItem> todos = Arrays.asList(new TodoItem("1", "abc", false, labels));
+        List<Label> labels = Collections.singletonList(new Label("1", "shopping", "white"));
+        List<TodoItem> todos = Collections.singletonList(new TodoItem("1", "abc", false, labels));
         Mockito.when(todoListRepository.findAll()).thenReturn(todos);
         //when
         List<TodoItem> actualTodos = todoListService.getAllTodoItems();
         //then
         assertEquals(todos, actualTodos);
+    }
+    @Test
+    public void should_return_todo_item_when_add_todo_given_a_todo_item() {
+        //given
+        TodoListRepository todoListRepository = Mockito.mock(TodoListRepository.class);
+        TodoListService todoListService = new TodoListService(todoListRepository);
+        List<Label> labels = Collections.singletonList(new Label("1", "shopping", "white"));
+        TodoItem todoItem = new TodoItem("1", "abc", false, labels);
+        Mockito.when(todoListRepository.save(todoItem)).thenReturn(todoItem);
+        //when
+        TodoItem actualTodo = todoListService.addTodoItem(todoItem);
+        //then
+        assertEquals(todoItem, actualTodo);
     }
 }
