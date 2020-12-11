e7d7cba5c20cde129bcf692873d0b5b05ef11d93
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-12-11 11:27:11 +0800

    [Spring-TodoList][Dominic Leung] feat: delete all related labelID in todo Item when delete label

--- src/main/java/com/thoughtwork/todoList/services/LabelService.java
+++ src/main/java/com/thoughtwork/todoList/services/LabelService.java
@@ -1,19 +1,24 @@
 package com.thoughtwork.todoList.services;
 
 import com.thoughtwork.todoList.entities.Label;
+import com.thoughtwork.todoList.entities.TodoItem;
 import com.thoughtwork.todoList.exceptions.LabelNotFoundException;
 import com.thoughtwork.todoList.repositories.LabelRepository;
+import com.thoughtwork.todoList.repositories.TodoListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class LabelService {
     private final String LABEL_NOT_FOUND = "LABEL_NOT_FOUND";
     @Autowired
     private LabelRepository labelRepository;
+    @Autowired
+    private TodoListRepository todoListRepository;
 
     public LabelService(LabelRepository labelRepository) {
         this.labelRepository = labelRepository;
@@ -34,6 +39,12 @@ public class LabelService {
         Optional<Label> labelToDelete = labelRepository.findById(labelID);
         if (labelToDelete.isPresent()) {
             labelRepository.deleteById(labelID);
+            todoListRepository.findAll().forEach(todoItem -> {
+                todoItem.setLabels(todoItem.getLabels().stream().
+                        filter(label-> !labelID.equals(label)).collect(Collectors.toList()));
+                todoListRepository.save(todoItem);
+
+            });
         } else {
             throw new LabelNotFoundException(LABEL_NOT_FOUND);
         }
